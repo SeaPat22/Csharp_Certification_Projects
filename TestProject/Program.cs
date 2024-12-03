ca43c95d6e0c93066f06747749c28cf86c9cfdfe
Sean King <89614014+SeaPat22@users.noreply.github.com>
2024-12-03 03:25:53 -0500

    Add files via upload

---
+++ TestProject/Program.cs
@@ -0,0 +1,13 @@
+﻿string permission = "Manager";
+int level = 65;
+
+if (permission.Contains("Admin")) {
+    Console.WriteLine((level > 55) ? "Welcome, Super Admin user." : "Welcome, Admin user.");
+}
+else if (permission.Contains("Manager")) {
+    Console.WriteLine((level >= 20) ? "Contact an admin for access." : "You do not have sufficient privileges.");
+}
+else if (!permission.Contains("Admin") && !permission.Contains("Manager")) {
+    Console.WriteLine("You do not have sufficient privileges.");
+}
+
